80d641ca873cb4f64c448571f188f160b0ef6d6f
Francisco Manzano Magaña <keoh77@gmail.com>
2018-04-18 23:48:17 +0200

    Added periods values

---
+++ src/incomes.ts
@@ -0,0 +1,17 @@
+const BIANNUAL = 2
+const MONTHLY = 12
+const ANNUAL = 1
+const QUADIMESTRAL = 3
+const QUARTERLY = 4
+const BIYEARLY = 0.5
+const BIMONTHLY = 6
+
+export default {
+    BIANNUAL,
+    MONTHLY,
+    ANNUAL,
+    QUADIMESTRAL,
+    QUARTERLY,
+    BIYEARLY,
+    BIMONTHLY
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 
 import _utils from './utils';
+import _values from './incomes';
+import _capitalization from './capitalization';
 
 export const utils = _utils
+export const values = _values;
+export const capitalization = _capitalization;
