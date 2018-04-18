f7d52c5dc719c08d8aad62c5230e4a9523b62f3e
Francisco Manzano Magaña <keoh77@gmail.com>
2018-04-18 23:49:58 +0200

    Removed capitalization

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 
 import _utils from './utils';
 import _values from './incomes';
-import _capitalization from './capitalization';
 
 export const utils = _utils
 export const values = _values;
-export const capitalization = _capitalization;
+
